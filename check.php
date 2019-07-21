d1def5c6710fde3d55550c5018f4b04e264eb46c
ScRipt1337 <26287773+ScRiPt1337@users.noreply.github.com>
2019-07-21 17:03:02 +0530

    Update check.php

--- check.php
+++ check.php
@@ -4,7 +4,7 @@ session_start();
        $password = "1337";
       if($_GET["username"] == $username || $_GET["password"] == $password) {
          $_SESSION['username'] = $username;
-         header("location: control.php");
+         header("location: index.php");
       }else {
           header("location: login.php");
       }
